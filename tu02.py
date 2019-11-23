f86b2ea655ba7501f24a71c9cadaf680852fe148
ishaan-rd <ishaanrd6@gmail.com>
2019-11-23 17:51:43 +0530

    adding first few files

---
+++ tu02.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get("https://akaunting.com/login")
+user = "ishaanrd@gmail.com"
+pwd = "6kDvRirr9i5"
+
+driver.find_element_by_xpath('//*[@id="email"]').send_keys(user)
+driver.find_element_by_xpath('//*[@id="password"]').send_keys(pwd)
+driver.find_element_by_xpath('//*[@id="login-form"]/div[3]/label/input').click()
+driver.find_element_by_id("recaptcha-anchor-label").click()
+driver.find_element_by_xpath('//*[@id="login-form-submit"]').click()
